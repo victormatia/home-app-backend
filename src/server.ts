c3c2937a9f9366862b8fdac23c8d750badc65a72
victormatia <fvictor1705@gmail.com>
2023-08-17 23:04:05 -0300

    refactor: standardize code

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
 const server = express();
 
 server.get('/', async (_req: Request, res: Response) => {
-  res.status(200).send('Ok')
+  res.status(200).send('Ok');
 });
 
 server.listen(3001, () => console.log('server is running on port 3001'));

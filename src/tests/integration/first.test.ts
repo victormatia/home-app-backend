76dc59386faa4f37bc15520968b53f5b8aa7d8c1
victormatia <fvictor1705@gmail.com>
2023-08-18 00:11:21 -0300

    tests: implement tool verification tests

---
+++ src/tests/integration/first.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('test implementation of testing tools - integration', () => {
+  it('integration test', () => {
+    expect(1).to.be.equal(1);
+  });
+});
---
+++ src/tests/unitary/second.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('test implementation of testing tools - unitary', () => {
+  it('unitary test', () => {
+    expect(1).to.be.equal(1);
+  });
+});

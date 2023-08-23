f5087a9dfedaaf037cc33cf6181d9ac3975b4525
victormatia <fvictor1705@gmail.com>
2023-08-23 17:29:50 -0300

    refactor: update console.log message

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
 
+
 const server = express();
 
 server.get('/', async (_req: Request, res: Response) => {
   res.status(200).send('Ok');
 });
 
-server.listen(3001, () => console.log('server is running on port 3001'));
+
+server.listen(3001, () => console.log('server is running on: http://localhost:3001'));

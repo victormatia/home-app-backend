b8ff74b0556ba02951662413e06637b49c766265
victormatia <fvictor1705@gmail.com>
2023-09-15 23:36:18 -0300

    feat: return token when a new user is registered

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,6 @@
 import { User, PrismaClient } from '@prisma/client';
 import IService from '../interfaces/IService';
+import Jwt from '../auth/Jwt';
 
 class UserService {
   private _model: PrismaClient;
@@ -8,11 +9,13 @@ class UserService {
     this._model = model;
   }
 
-  public async create(infos: User): Promise<IService<User>> {
+  public async create(infos: User): Promise<IService<string>> {
     try {
-      const result = await this._model.user.create({data: infos});
+      await this._model.user.create({data: infos});
 
-      return {result};
+      const token = Jwt.createToken({...infos});
+
+      return { result: token };
 
     } catch (e) {
       console.error(e);

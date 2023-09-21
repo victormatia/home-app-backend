9f6a12627ab9f79388ff49f57f237e1b9143c97a
victormatia <fvictor1705@gmail.com>
2023-09-21 15:35:11 -0300

    feat: creates getAllUsers method to UserController class

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -14,6 +14,13 @@ class UserController {
     return message ? res.status(400).json({ message }) 
       : res.status(201).json({ token: result });
   }; 
+
+  public getAllUsers: RequestHandler = async (_req, res) => {
+    const { message, result } = await this._service.getAllUsers();
+
+    return message ? res.status(400).json({ message }) 
+      : res.status(200).json(result);
+  };
 }
 
 export default UserController;

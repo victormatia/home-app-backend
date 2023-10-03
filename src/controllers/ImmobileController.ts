764ad0c71ac16f67c5a616167990b9fd6fa81360
victormatia <fvictor1705@gmail.com>
2023-10-02 23:57:59 -0300

    feat: creates ImmobileController class

---
+++ src/controllers/ImmobileController.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express';
+import ImmobileService from '../services/ImmobileService';
+
+class ImmobileController {
+  private _service: ImmobileService;
+  
+  constructor(service: ImmobileService) {
+    this._service = service;
+  }
+
+  public create: RequestHandler = async (req, res) => {
+    const  { message, result } = await this._service.create(req.body);
+
+    return message ? res.status(400).json({ message }) 
+      : res.status(201).json(result);
+  }; 
+}
+
+export default  ImmobileController;

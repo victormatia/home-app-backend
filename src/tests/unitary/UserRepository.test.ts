31c505efc180c572e1f9d61e376eff37bfa15ab6
lucas-morais <lucasmoraisss@gmail.com>
2023-12-12 11:24:57 -0300

    test: testing service function getAll

--- src/tests/unitary/UserRepository.test.ts
+++ src/tests/unitary/UserRepository.test.ts
@@ -19,18 +19,40 @@ describe('Testing User Repository Layer - Unit Test', () => {
   });
   describe('Testing repository create method', () => {
     
-    
     it('Should create a user successfully', async () => {
       const userCreatedMock = MockGenerator.generateFakeUser();
+      const createUserDto = {} as CreateUserDTO;
+      
       prismaMock.user.create.resolves(userCreatedMock);
       
-      const userCreated = await userRepository.create({} as CreateUserDTO);
+      const userCreated = await userRepository.create(createUserDto);
       
-      expect(prismaMock.user.create.calledOnce).to.be.true;
-    
+      expect(prismaMock.user.create.calledWith({data: createUserDto})).to.be.true;
       expect(userCreated).to.be.deep.equal(userCreatedMock);
 
     });
+  });
 
+  describe('Tesing repository getAll methdod', () => {
+    it('Should return a list of users', async () => {
+      const lengthOfList = 5;
+      const userListMock = MockGenerator.generateListOfFakeUsers(lengthOfList);
+
+      prismaMock.user.findMany.resolves(userListMock);
+
+      const listOfUsers = await userRepository.getAll();
+
+      expect(prismaMock.user.findMany.calledOnce).to.be.true;
+      expect(listOfUsers).to.have.length(lengthOfList);
+      listOfUsers.forEach((user)=> {
+        expect(user).to.be.have.property('id');
+        expect(user).to.be.have.property('name');
+        expect(user).to.be.have.property('email');
+        expect(user).to.be.have.property('password');
+        expect(user).to.be.have.property('birthDate');
+        expect(user).to.be.have.property('createdAt');
+        expect(user).to.be.have.property('updatedAt');
+      });
+    });
   });
 });

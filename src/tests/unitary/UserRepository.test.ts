d79cfe80c90a21f4f65229ce8cbeb8f5ea48d4d3
lucas-morais <lucasmoraisss@gmail.com>
2023-12-12 12:38:50 -0300

    feat: testing login route

--- src/tests/unitary/UserRepository.test.ts
+++ src/tests/unitary/UserRepository.test.ts
@@ -7,7 +7,7 @@ import MockGenerator from '../util/MockGenerator';
 import prismaMock from '../util/prismaMock';
 
 describe('Testing User Repository Layer - Unit Test', () => {
-  
+
   let userRepository: UserRepository;
 
   before(() => {
@@ -18,16 +18,16 @@ describe('Testing User Repository Layer - Unit Test', () => {
     sinon.restore();
   });
   describe('Testing repository create method', () => {
-    
+
     it('Should create a user successfully', async () => {
       const userCreatedMock = MockGenerator.generateFakeUser();
       const createUserDto = {} as CreateUserDTO;
-      
+
       prismaMock.user.create.resolves(userCreatedMock);
-      
+
       const userCreated = await userRepository.create(createUserDto);
-      
-      expect(prismaMock.user.create.calledWith({data: createUserDto})).to.be.true;
+
+      expect(prismaMock.user.create.calledWith({ data: createUserDto })).to.be.true;
       expect(userCreated).to.be.deep.equal(userCreatedMock);
 
     });
@@ -44,7 +44,7 @@ describe('Testing User Repository Layer - Unit Test', () => {
 
       expect(prismaMock.user.findMany.calledOnce).to.be.true;
       expect(listOfUsers).to.have.length(lengthOfList);
-      listOfUsers.forEach((user)=> {
+      listOfUsers.forEach((user) => {
         expect(user).to.be.have.property('id');
         expect(user).to.be.have.property('name');
         expect(user).to.be.have.property('email');
@@ -53,6 +53,27 @@ describe('Testing User Repository Layer - Unit Test', () => {
         expect(user).to.be.have.property('createdAt');
         expect(user).to.be.have.property('updatedAt');
       });
+    });
+  });
+  describe('Testing repository getUserByIdMethod', () => {
+    it('Should return a user if does exist', async () => {
+      const mockedUser = MockGenerator.generateFakeUser();
+
+      prismaMock.user.findUnique.resolves(mockedUser);
+
+      const user = await userRepository.getUserById(mockedUser.id);
+
+      expect(prismaMock.user.findUnique.calledOnceWith({ where: { id: mockedUser.id } }));
+      expect(user?.id).to.be.equal(mockedUser.id);
+    });
+    it('Should return null if user does not exist', async () => {
+      const fakeId = MockGenerator.generateFakeId();
+      prismaMock.user.findUnique.resolves(null);
+
+      const user = await userRepository.getUserById(fakeId);
+
+      expect(prismaMock.user.findUnique.calledOnceWith({ where: { id: fakeId} }));
+      expect(user).to.be.null;
     });
   });
 });

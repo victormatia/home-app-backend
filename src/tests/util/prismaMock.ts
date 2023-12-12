558fada90dfa71647595313ebab436d761522d1b
lucas-morais <lucasmoraisss@gmail.com>
2023-12-12 11:23:31 -0300

    feat: add stub to prisma function findMany

--- src/tests/util/prismaMock.ts
+++ src/tests/util/prismaMock.ts
@@ -1,7 +1,12 @@
 import sinon from 'sinon';
 import { prisma } from '../../routes/user.routes';
 
-const prismaMock = {user: {create: sinon.stub(prisma.user, 'create')}};
+const prismaMock = { 
+  user: { 
+    create: sinon.stub(prisma.user, 'create'), 
+    findMany: sinon.stub(prisma.user, 'findMany'), 
+  }, 
+};
 
 
 export default prismaMock;

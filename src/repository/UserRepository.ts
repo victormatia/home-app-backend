468b7aea509ac79cf2819408e3f18e77e50412fb
lucas-morais <lucasmoraisss@gmail.com>
2023-12-12 11:25:34 -0300

    style: correcting styles erros

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -4,23 +4,22 @@ import { CreateUserDTO } from '../interfaces/UserDto';
 
 
 class UserRepository {
-  private _model:  PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
-  
+  private _model: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+
   constructor(prismaCliente: PrismaClient) {
     this._model = prismaCliente;
   }
 
   async create(data: CreateUserDTO): Promise<User> {
-    return await this._model.user.create({data});
+    return await this._model.user.create({ data });
   }
 
-  async getAll():Promise<User[]> {
+  async getAll(): Promise<User[]> {
     return this._model.user.findMany();
   }
-  async findUnique( id: string ): Promise<User | null> {
-    return this._model.user.findUnique({where: {id}});
+  async getUserById(id: string): Promise<User | null> {
+    return this._model.user.findUnique({ where: { id } });
   }
 }
-
 
 export default UserRepository;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -5,7 +5,7 @@ import { CreateUserDTO } from '../interfaces/UserDto';
 import UserRepository from '../repository/UserRepository';
 
 class UserService {
-  
+
 
   constructor(private _repository: UserRepository) {
   }
@@ -14,7 +14,7 @@ class UserService {
     try {
       await this._repository.create(data);
 
-      const token = Jwt.createToken({...data});
+      const token = Jwt.createToken({ ...data });
 
       return { result: token };
 
@@ -24,23 +24,23 @@ class UserService {
     }
   }
 
-  public async getAll(): Promise<IService<User[]>> { 
+  public async getAll(): Promise<IService<User[]>> {
     try {
       const users = await this._repository.getAll();
 
       return { result: users };
 
     } catch (e) {
-      console.error(e);
+      // console.error(e);
       return { message: 'Something went wrong' };
     }
   }
 
   public async login(userId: string): Promise<IService<string>> {
     try {
-      const user = await this._repository.findUnique(userId);
+      const user = await this._repository.getUserById(userId);
 
-      if(!user) {
+      if (!user) {
         return { message: 'User not found' };
       }
 
@@ -49,7 +49,7 @@ class UserService {
       return { result: token };
 
     } catch (e) {
-      console.error(e);
+      // console.error(e);
       return { message: 'Something went wrong, user not found' };
     }
   }

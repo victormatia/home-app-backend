3aa93a27462c3aef226ad04f8ec42c9c38b6cccb
lucas-morais <lucasmoraisss@gmail.com>
2023-12-12 11:24:34 -0300

    test: testing repository function getAll

--- src/tests/unitary/UserService.test.ts
+++ src/tests/unitary/UserService.test.ts
@@ -5,13 +5,14 @@ import Jwt from '../../auth/Jwt';
 import { CreateUserDTO } from '../../interfaces/UserDto';
 import UserRepository from '../../repository/UserRepository';
 import UserService from '../../services/UserService';
+import MockGenerator from '../util/MockGenerator';
 
 describe('Testing Service Layer - Unit Test', () => {
 
   let userService: UserService;
   before(() => {
     const userRepository = new UserRepository({} as PrismaClient);
-    
+
     userService = new UserService(userRepository);
   });
 
@@ -20,17 +21,17 @@ describe('Testing Service Layer - Unit Test', () => {
   });
 
   describe('Testing service create method', () => {
-    
+
     it('Should return a json web token if user is created', async () => {
       const tokenMock = 'Token mock';
-      
+
       const createUserStub = sinon.stub(UserRepository.prototype, 'create').resolves();
       const generateTokenStub = sinon.stub(Jwt, 'createToken').returns(tokenMock);
-      
+
       const serviceResponse = await userService.create({} as CreateUserDTO);
 
-      sinon.assert.calledOnce(createUserStub);
-      sinon.assert.calledOnce(generateTokenStub);
+      expect(createUserStub.calledOnce).to.be.true;
+      expect(generateTokenStub.calledOnce).to.be.true;
 
 
       expect(serviceResponse.result).to.be.equal(tokenMock);
@@ -39,11 +40,33 @@ describe('Testing Service Layer - Unit Test', () => {
       const createUserStub = sinon.stub(UserRepository.prototype, 'create').rejects();
 
       const errorMessage = await userService.create({} as CreateUserDTO);
-      sinon.assert.calledOnce(createUserStub);
 
+      expect(createUserStub.calledOnce).to.be.true;
       expect(errorMessage.message).to.be.equal('Something went wrong, user was not registered');
-      
+
     });
 
+  });
+  describe('Tesing service getAll methdod', () => {
+    it('Should return a list of users', async () => {
+      const lengthOfList = 5;
+      const userListMock = MockGenerator.generateListOfFakeUsers(lengthOfList);
+
+      const getAllUsersStub = sinon.stub(UserRepository.prototype, 'getAll').resolves(userListMock);
+
+      const errorMessage = await userService.getAll();
+
+      expect(getAllUsersStub.calledOnce).to.be.true;
+      expect(errorMessage.result).to.be.deep.equal(userListMock);
+
+    });
+    it('Should return a erro message if is not possible to get a list of users', async () => {
+      const getAllUsersStub = sinon.stub(UserRepository.prototype, 'getAll').rejects();
+
+      const errorMessage = await userService.getAll();
+
+      expect(getAllUsersStub.calledOnce).to.be.true;
+      expect(errorMessage.message).to.be.equal('Something went wrong');
+    });
   });
 });

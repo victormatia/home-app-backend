8ebf9f925314dc00706cbb8e59f758148aee98c5
Lucas Toledo <lucasmoraisss@gmail.com>
2024-01-25 10:47:33 -0300

    feat: implementing authentication middleware

--- src/auth/Auth.ts
+++ src/auth/Auth.ts
@@ -1,40 +1,35 @@
-import { NextFunction, Request, Response } from 'express';
-import { JWTPayload, auth, claimCheck } from 'express-oauth2-jwt-bearer';
-import { PermissionEnum } from './PermissionEnum';
+import { config } from 'dotenv';
+import { auth } from 'express-oauth2-jwt-bearer';
+config();
 
-export class Auth {
-  static authenticationMiddleware() {
-    return auth({
-      audience: process.env.AUDIENCE,
-      issuerBaseURL: process.env.ISSUER,
-      tokenSigningAlg: process.env.TOKEN_ALGORITHM || 'RS256',
-    });
-  }
-  
-  static permissionMiddleware(requiredPermisons: PermissionEnum[]) {
-    return (req: Request, res: Response, next: NextFunction) => {
-      const permissionsCheck = claimCheck((claims) => {
+export const authMiddleware = auth({
+  audience: process.env.AUDIENCE,
+  issuerBaseURL: process.env.ISSUER,
+});
+//   static permissionMiddleware(requiredPermisons: PermissionEnum[]) {
+//     return (req: Request, res: Response, next: NextFunction) => {
+//       const permissionsCheck = claimCheck((claims) => {
         
-        const permissions = claims.permissions as PermissionEnum[];
-        const hasPermission = permissions.some((permission) => {
-          if(requiredPermisons.includes(permission)) {
-            return this.checkPermissionRule(permission, req, claims); 
-          }
-          return false;
-        });
+//         const permissions = claims.permissions as PermissionEnum[];
+//         const hasPermission = permissions.some((permission) => {
+//           if(requiredPermisons.includes(permission)) {
+//             return this.checkPermissionRule(permission, req, claims); 
+//           }
+//           return false;
+//         });
         
-        return hasPermission;
-      });
-      permissionsCheck(req, res, next);
-    };
-  }
+//         return hasPermission;
+//       });
+//       permissionsCheck(req, res, next);
+//     };
+//   }
 
-  static checkPermissionRule(permission: PermissionEnum, req: Request, claims: JWTPayload) {
-    if(permission === PermissionEnum.ADMIN) {
-      return true;
-    } else if(permission === PermissionEnum.USER) {
-      return req?.params.id === claims?.sub;
-    }
-    return false;
-  }
-}
+//   static checkPermissionRule(permission: PermissionEnum, req: Request, claims: JWTPayload) {
+//     if(permission === PermissionEnum.ADMIN) {
+//       return true;
+//     } else if(permission === PermissionEnum.USER) {
+//       return req?.params.id === claims?.sub;
+//     }
+//     return false;
+//   }
+// }

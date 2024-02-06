759e57954239c9ff982eb82e7ba24687bf2ad273
Lucas Toledo <lucasmoraisss@gmail.com>
2024-02-06 09:51:46 -0300

    chore: add error log -temporary

--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -6,7 +6,8 @@ export function errorMiddleware(err: Error, _req: Request, res: Response, _next:
   if(err instanceof ApiError) {
     console.log(err);
     return res.status(err.status).json({message: err.message});
-  } 
+  }
+  console.log(err);
   return res.status(500).json({ message: GenericErrors.SERVER_ERROR});
 }
 

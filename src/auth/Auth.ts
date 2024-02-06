b59e9d7e3a0d31e57856c07c696a544ee8a1a219
Lucas Toledo <lucasmoraisss@gmail.com>
2024-02-06 09:49:15 -0300

    feat: implement permission middleware

--- src/auth/Auth.ts
+++ src/auth/Auth.ts
@@ -2,6 +2,8 @@ import { ManagementClient } from 'auth0';
 import { config } from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
 import { auth, claimCheck } from 'express-oauth2-jwt-bearer';
+import { PermissionContainer } from './PermissionContainer';
+import { PermissionEnum } from './PermissionEnum';
 config();
 
 export const authMiddleware = auth({
@@ -10,21 +12,25 @@ export const authMiddleware = auth({
 });
 
 export const managementClient = new ManagementClient({
-  domain: 'dev-xxqg1s11rae0x4rz.us.auth0.com',
-  clientId: '9c5ruxxi9CEcIoaVk5Js02oJ6Q7rqHPP',
-  clientSecret: 'asRMXlKS_xnwU23F7HjNV0SdW42pPcVdCk6rKKMkS3r_sAJIn9eANngdTRUyEcg4',
+  domain: process.env.AUTH_DOMAIN as string,
+  clientId: process.env.AUTH_CLIENT_ID as string,
+  clientSecret: process.env.AUTH_SECRET as string,
 });
 
 
-export const checkRequiredPermissions = () => {
+export const checkRequiredPermissions = (permissions: PermissionEnum[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const permissionCheck = claimCheck((payload) => {
-      const permissions = payload.permissions as string[];
-
-      console.log(payload);
-      console.log(permissions);
-      return true;
-
+      const calimPermissions = payload.permissions as PermissionEnum[];
+      const permission = calimPermissions[0];
+      if(permissions.includes(permission)) {
+        const tokenUserId = payload.userId as string;
+        const userId = req.params.id;
+        const permissionContainer = new PermissionContainer(userId, tokenUserId);
+        return permissionContainer.useValidator(permission);
+      }
+      
+      return false;
     });
 
     permissionCheck(req, res, next);

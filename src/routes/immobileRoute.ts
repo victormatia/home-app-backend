fe6c50ebb9e58b9e76189d9edcc4cf2b65e0d17d
Lucas Toledo <lucasmoraisss@gmail.com>
2024-02-06 13:01:25 -0300

    feat: add permission middlewares to immobile route

--- src/routes/immobileRoute.ts
+++ src/routes/immobileRoute.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { Router } from 'express';
+import { authMiddleware, checkRequiredPermissions } from '../auth/Auth';
+import { PermissionEnum } from '../auth/PermissionEnum';
 import ImmobileController from '../controllers/ImmobileController';
 import ImmobileRepository from '../repository/ImmobileRepository';
 import ImmobileService from '../services/ImmobileService';
@@ -13,9 +15,19 @@ const service = new ImmobileService(repository);
 const controller = new ImmobileController(service);
 
 router.get('/list', controller.getAll);
-router.post('/create', controller.create);
+router.post('/create', authMiddleware, controller.create);
 router.get('/list/:id', controller.getImmobileById);
-router.delete('/delete/:id', controller.deleteImmobileById);
-router.patch('/update/:id', controller.updateImmobileById);
+router.delete(
+  '/delete/:id', 
+  authMiddleware,
+  checkRequiredPermissions([PermissionEnum.ADMIN, PermissionEnum.IMMOBILE]),
+  controller.deleteImmobileById,
+);
+router.patch(
+  '/update/:id', 
+  authMiddleware,
+  checkRequiredPermissions([PermissionEnum.ADMIN, PermissionEnum.IMMOBILE]),
+  controller.updateImmobileById,
+);
 
 export default router;

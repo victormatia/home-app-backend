5d2a5348739286c04ccac30c69649684d5e2a037
Lucas Toledo <lucasmoraisss@gmail.com>
2024-02-07 18:16:04 -0300

    feat: add new condtion to check unauthorized error

--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
+import { UnauthorizedError } from 'express-oauth2-jwt-bearer';
 import { ApiError } from '../error/ApiError';
-import { GenericErrors } from '../util/messages';
+import { AuthorizationErrors, GenericErrors } from '../util/messages';
 
 export function errorMiddleware(err: Error, _req: Request, res: Response, _next: NextFunction) {
   if(err instanceof ApiError) {
     console.log(err);
     return res.status(err.status).json({message: err.message});
+  }
+  if(err instanceof UnauthorizedError) {
+    return res.status(err.statusCode).json({message: AuthorizationErrors.UNAUTHORIZED_ERROR});
   }
   console.log(err);
   return res.status(500).json({ message: GenericErrors.SERVER_ERROR});

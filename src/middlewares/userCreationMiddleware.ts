683c95571e25ca618189bfd9850a7691b2a92e75
Lucas Toledo <lucasmoraisss@gmail.com>
2024-02-07 18:16:24 -0300

    feat: add middlewar to capture user id from token

--- src/middlewares/userCreationMiddleware.ts
+++ src/middlewares/userCreationMiddleware.ts
@@ -1,10 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
+import { JWTPayload } from 'express-oauth2-jwt-bearer';
 import { decode } from 'jsonwebtoken';
+import { BadRequestError } from '../error/BadRequestError';
+import { UserErrors } from '../util/messages';
 
 export function userCreationMiddleware(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization?.split(' ')[1] as string;
-  const info = decode(token, {complete: true});
-  req.body = {...req.body, id:'TESTE'};
-  console.log(info);
+  try {
+    const token = req.headers.authorization?.split(' ')[1] as string;
+    const userInfo = decode(token, { complete: true });
+    const payload = userInfo?.payload as JWTPayload;
+    const { userId } = payload as { userId: string };
+    req.body = { ...req.body, id: userId };
+
+  } catch (err) {
+    throw new BadRequestError(UserErrors.TOKEN_MALFORMED);
+  }
   return next();
 }

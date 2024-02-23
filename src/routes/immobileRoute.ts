ce303ef2b11c8f37537f137c444314d97cfecc86
lucas-morais <lucasmoraisss@gmail.com>
2024-02-23 16:57:06 -0300

    refactor: add favorite immobile rep instance and changes routes specification

--- src/routes/immobileRoute.ts
+++ src/routes/immobileRoute.ts
@@ -1,8 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { Router } from 'express';
-import { authMiddleware, checkRequiredPermissions } from '../auth/Auth';
-import { PermissionEnum } from '../auth/PermissionEnum';
 import ImmobileController from '../controllers/ImmobileController';
+import { FavoriteRepository } from '../repository/FavoriteRepository';
 import ImmobileRepository from '../repository/ImmobileRepository';
 import ImmobileService from '../services/ImmobileService';
 
@@ -11,23 +10,17 @@ const router = Router();
 const prisma = new PrismaClient();
 
 const repository = new ImmobileRepository(prisma);
-const service = new ImmobileService(repository);
+const favoriteRepository = new FavoriteRepository(prisma);
+const service = new ImmobileService(repository, favoriteRepository);
 const controller = new ImmobileController(service);
 
 router.get('/list', controller.getAll);
-router.post('/create', authMiddleware, controller.create);
-router.get('/list/:id', controller.getImmobileById);
-router.delete(
-  '/delete/:id', 
-  authMiddleware,
-  checkRequiredPermissions([PermissionEnum.ADMIN, PermissionEnum.IMMOBILE]),
-  controller.deleteImmobileById,
-);
-router.patch(
-  '/update/:id', 
-  authMiddleware,
-  checkRequiredPermissions([PermissionEnum.ADMIN, PermissionEnum.IMMOBILE]),
-  controller.updateImmobileById,
-);
+router.post('/create', controller.create);
+router.get('/list/id', controller.getImmobileById);
+router.delete('/delete/id', controller.deleteImmobileById);
+router.patch('/update/id', controller.updateImmobileById);
+router.post('/favorite', controller.favoriteImmobile);
+router.delete('/unfavorite', controller.unfavoriteImmobile);
+
 
 export default router;
